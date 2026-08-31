1c6b4dba48111a620c8768767430bba718a0d1d3
Mohamed Atef <proofesser@gmail.com>
2026-08-31 20:02:02 +0200

    resolved till Q - 13

--- src/Operators.java
+++ src/Operators.java
@@ -242,16 +242,203 @@ public class Operators {
 //        for (int i = 0; i < fruits.length; i++) {
 //            System.out.println(fruits[i] + " number: " + numbers[i]);
 //        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String[] fruits = new String[10];
+//        for (int i = 0; i < fruits.length; i++) {
+//            System.out.println("Enter Fruit Name");
+//            fruits[i] = scanner.nextLine();
+//        }
+//
+//        for (String fruit: fruits){
+//            System.out.println(fruit);
+//        }
 
-        Scanner scanner = new Scanner(System.in);
-        String[] fruits = new String[10];
-        for (int i = 0; i < fruits.length; i++) {
-            System.out.println("Enter Fruit Name");
-            fruits[i] = scanner.nextLine();
-        }
+        /*
+        Task 1 - Student Information - solution
+         */
+//        int age = 0;
+//        int grade = 0;
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your age");
+//        age =  scanner.nextInt();
+//        System.out.println("Your age is: " + age);
+//
+//        System.out.println("Enter your grade");
+//        grade =  scanner.nextInt();
+//        System.out.println("Your grade is: " + grade);
+//
+//        System.out.println("Did you pass the exams? (yes/no)");
+//        String answer = scanner.next();
+//
+//        if (answer.equalsIgnoreCase("yes")) {
+//            System.out.println("You passed the exam");
+//        } else {
+//            System.out.println("You failed the exam");
+//        }
 
-        for (String fruit: fruits){
-            System.out.println(fruit);
+        /*
+        Task 2 - Numeric Calculations - solution
+         */
+//
+//        int x = 25, y = 8;
+//        System.out.println(x + y);
+//        System.out.println(x - y);
+//        System.out.println(x * y);
+//        System.out.println(x / y);
+//        System.out.println(x % y);
+        /*
+        integer division left a 1 in the reminder which has not been printed using the division directly. Instead, we had to use the % operator.
+         */
+
+        /*
+        Task 3 - Primitive Type Selection - solution
+         */
+//
+//        int studentsNumber = 0;
+//        long population = 8_000_000_000L;
+//        double price = 30.5;
+//        float temperature = 26.5f;
+//        char grade = 'A';
+//        boolean isLoggedIn = true;
+
+        /*
+        Task 4 - String Immutability - Solution
+         */
+//
+//        String name = "Ahmed";
+//        String newName;
+//        newName = name.concat("Ali");
+//        System.out.println(name);
+//        System.out.println(newName);
+
+        /*
+        Task 5 - StringBuilder Mutability - Solution
+         */
+//
+//        StringBuilder stringBuilder = new StringBuilder("Java");
+//        stringBuilder.append("Programming");
+//        stringBuilder.append(" - Session One");
+//        System.out.println(stringBuilder);
+
+        /*
+        Task 6 - Mutable Array Reference
+         */
+//
+//        int [] numbers = {10, 20, 30};
+//        int [] nums = numbers;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
+//
+//        nums[0] = 500;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
+
+        /*
+        3. One-Dimensional Arrays
+        Task 7 - Array Basics
+         */
+
+//        int [] nums = {5, 10, 15, 20, 25};
+//        System.out.println("First value: " + nums[0]);
+//        System.out.println("Fourth value: " + nums[3]);
+//        System.out.println("Length: " + nums.length);
+
+        /*
+        Task 8 - Update Array Elements
+         */
+//        int [] numbers = {10, 20, 30, 40, 50};
+//        numbers[1] = 200;
+//        numbers[3] = 400;
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
+
+        /*
+        Task 9 - Array Sum and Average
+         */
+//
+//        int[] numbers = {12, 18, 25, 30, 15};
+//        int sum = 0;
+//        int average = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            sum += numbers[i];
+//        }
+//        System.out.println("Total = " + sum);
+//
+//        average = sum / numbers.length;
+//        System.out.println("Average = " + average);
+
+        /*
+        Task 10 - Even and Odd Numbers
+         */
+//
+//        for (int i = 1; i <=20 ; i++) {
+//            if ( i % 2 == 0){
+//                System.out.println(i + " is Even");
+//            }else {
+//                System.out.println(i + " is Odd");
+//            }
+//        }
+
+        /*
+        Task 11 - Find the Largest Array Value
+         */
+//
+//        int[] numbers = {17, 42, 8, 91, 35, 60};
+//        int largest = numbers[0];
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] > largest){
+//                largest = numbers[i];
+//            }
+//        }
+//        System.out.println(largest + " is the largest number");
+
+        /*
+        Task 12 - Count Passing Grades
+         */
+//
+//        int[] grades = {45, 80, 72, 30, 65, 90, 50};
+//        int sumPassed = 0;
+//        int sumFailed = 0;
+//        for (int i = 0; i < grades.length; i++) {
+//            if (grades[i] >= 50){
+//                sumPassed++;
+//            }else {
+//                sumFailed++;
+//            }
+//        }
+//
+//        System.out.println("Number of Passed students is: " +
+//                sumPassed + ", and Number of Failed students is: "
+//                + sumFailed);
+
+        /*
+        Task 13 - Count with while
+         */
+//
+//        int counter = 0;
+//        while (counter < 10){
+//            counter ++;
+//            System.out.println(counter);
+//        }
+//        System.out.println("finished");
+
+        /*
+        Task 14 - Sum Until a Limit
+         */
+
+        int limit = 50;
+        int sum = 1;
+        while (sum < limit){
+            sum+= sum;
+            sum++;
         }
+        System.out.println(sum);
     }
 }
